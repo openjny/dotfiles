71ad5c0016a6c13ddb772301e013e8e1fa8719e2
OpenJNY <openjny@gmail.com>
2022-08-13 17:34:07 +0900

    change: distinguish INSERT/NORMAL mode in prompt

--- zsh/.zsh/01_prompt.zsh
+++ zsh/.zsh/01_prompt.zsh
@@ -14,3 +14,20 @@ export ZSH_THEME="agnoster"
 
 # username@hostname -> empty
 prompt_context() {}
+
+# differenciate prompt between in INSERT/NOMARL modes
+function zle-line-init zle-keymap-select {
+  case $KEYMAP in
+    vicmd)
+        prompt_context() {
+            prompt_segment black default "📣 "
+        }
+    ;;
+    main|viins)
+        prompt_context() {}
+    ;;
+  esac
+  zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
